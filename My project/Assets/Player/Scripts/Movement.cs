47a3fa62a882637127502c780a0c9b07a549761a
ReFlex <dreaded@bk.ru>
2024-10-04 11:31:51 +0900

    Заряженная атака
    Пока без текстуры

--- My project/Assets/Player/Scripts/Movement.cs
+++ My project/Assets/Player/Scripts/Movement.cs
@@ -18,6 +18,9 @@ public class Movement : MonoBehaviour
     private bool isRun = false;
     private bool isAttacking = false;
 
+    public GameObject fireballPrefab; // Префаб огненного шара
+    public float fireballSpeed = 10f; // Скорость огненного шара
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -32,15 +35,61 @@ public class Movement : MonoBehaviour
     void Update()
     {
         HandleAttack();
-
+        HandleMagicAttack();
     }
 
-    public void MoveCharRight()
+    private void HandleMagicAttack()
     {
-        transform.position += new Vector3(2f, 0, 0);
-        Debug.Log("1!");
+        if (Input.GetKeyDown(KeyCode.Mouse0) && !isAttacking)
+        {
+            Debug.Log("Атака магией начата!");
+            isAttacking = true;
+            animator.SetBool("IsCharge", true);
+
+            // Выпускаем огненный шар
+            
+
+            StartCoroutine(ResetMagicAttack());
+        }
     }
-    
+
+    public float fireballSpawnOffset = 1f; // Смещение от игрока для появления огненного шара
+
+    private void ShootFireball()
+    {
+        // Получаем позицию курсора в мире
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0f; // Обнуляем ось Z, чтобы работать в 2D
+
+        // Вычисляем направление от игрока к курсору
+        Vector3 direction = (mousePosition - transform.position).normalized;
+
+        // Определяем позицию появления огненного шара с небольшим смещением от игрока
+        Vector3 fireballSpawnPosition = transform.position + direction * fireballSpawnOffset;
+
+        // Создаем огненный шар на этой позиции
+        GameObject fireball = Instantiate(fireballPrefab, fireballSpawnPosition, Quaternion.identity);
+
+        // Задаем движение огненного шара в направлении курсора
+        fireball.GetComponent<Rigidbody2D>().isKinematic = true; // Отключаем физику
+        fireball.GetComponent<Rigidbody2D>().velocity = direction * fireballSpeed;
+
+        Destroy(fireball, 30f);
+    }
+
+    private IEnumerator ResetMagicAttack()
+    {
+
+        yield return new WaitForSeconds(1f);
+        ShootFireball();
+        Debug.Log("Атака магией завершена!");
+        isAttacking = false;
+
+        animator.SetBool("IsCharge", false);
+
+    }
+
+
 
     private void HandleAttack()
     {
